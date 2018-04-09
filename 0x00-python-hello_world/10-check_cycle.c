0f607e14d0f38b1490a9be13dc5961f88491e7e1
Sidney Burkett <sidneyriffic@gmail.com>
2018-04-09 13:41:08 -0700

    Fix style on 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * check_cycle - checks if listint_t list loops on itself
+ *
+ * @list: list to check for loops
+ *
+ * Return: 1 if loops, 0 if not
+ */
 int check_cycle(listint_t *list)
 {
 	listint_t *ptr = list, *end;
@@ -17,7 +24,7 @@ int check_cycle(listint_t *list)
 		while (ptr != end)
 		{
 			if (ptr == end->next)
-				return(1);
+				return (1);
 			ptr = ptr->next;
 		}
 	}

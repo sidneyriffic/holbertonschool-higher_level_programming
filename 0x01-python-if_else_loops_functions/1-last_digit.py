1836dd78f9ec67ac96a27ee0ce952b39f120ddd3
Sidney Burkett <sidneyriffic@gmail.com>
2018-04-10 14:53:15 -0700

    Rework handling negative numbers for python style modulus

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number > 5:
+if abs(number) % 10 > 5:
     print("Last digit of", number, "is",
-          str(number)[-1], "and is greater than 5")
+          abs(number) % 10, "and is greater than 5")
 elif number == 0:
     print("Last digit of", number, "is",
-          str(number)[-1], "and is 0")
+          abs(number) % 10, "and is 0")
 else:
     print("Last digit of", number, "is",
-          str(number)[-1], "and is less than 6 and not 0")
+          abs(number) % 10, "and is less than 6 and not 0")

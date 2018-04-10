5e0eefaeb7bf5bebe26e37e1c4ead53201db9bed
Sidney Burkett <sidneyriffic@gmail.com>
2018-04-10 14:54:20 -0700

    Fix 0 case

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 if abs(number) % 10 > 5:
     print("Last digit of", number, "is",
           abs(number) % 10, "and is greater than 5")
-elif number == 0:
+elif abs(number) % 10 == 0:
     print("Last digit of", number, "is",
           abs(number) % 10, "and is 0")
 else:

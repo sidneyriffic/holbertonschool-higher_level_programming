a9aea95d622102dc98f9eab23f5a1ecc22d3e649
Sidney Burkett <sidneyriffic@gmail.com>
2018-04-10 15:47:24 -0700

    Use .format inside print for 1 to test checker

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+print("Last digit of {:d} is {:d}".format(number, abs(number) % 10),
+      end=' and is ')
 if abs(number) % 10 > 5:
-    print("Last digit of", number, "is",
-          abs(number) % 10, "and is greater than 5")
+    print("{}".format("greater than 5"))
 elif abs(number) % 10 == 0:
-    print("Last digit of", number, "is",
-          abs(number) % 10, "and is 0")
+    print("{}".format("0"))
 else:
-    print("Last digit of", number, "is",
-          abs(number) % 10, "and is less than 6 and not 0")
+    print("{}".format("less than 6 and not 0"))

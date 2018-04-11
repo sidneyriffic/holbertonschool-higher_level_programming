f063fbc1c9a3b265557e85f8404fe18fde8a2ae8
Sidney Burkett <sidneyriffic@gmail.com>
2018-04-10 22:19:29 -0700

    Use negative last digit for negative numbers in 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print("Last digit of {:d} is {:d}".format(number, abs(number) % 10),
+digit = number % 10
+if number < 10:
+    digit = digit - 10
+print("Last digit of {:d} is {:d}".format(number, digit),
       end=' and is ')
-if abs(number) % 10 > 5:
+if digit > 5:
     print("{}".format("greater than 5"))
-elif abs(number) % 10 == 0:
+elif digit == 0:
     print("{}".format("0"))
 else:
     print("{}".format("less than 6 and not 0"))

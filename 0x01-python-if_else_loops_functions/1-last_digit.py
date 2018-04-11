50b99d17809c4b6bfd967705662c8b06eff8fb9b
Sidney Burkett <sidneyriffic@gmail.com>
2018-04-10 22:25:44 -0700

    Optimize digit getting for - sign in 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-digit = number % 10
-if number < 0 and digit != 0:
-    digit = digit - 10
+if number >= 0:
+    digit = number % 10
+else:
+    digit = number % -10
 print("Last digit of {:d} is {:d}".format(number, digit),
       end=' and is ')
 if digit > 5:

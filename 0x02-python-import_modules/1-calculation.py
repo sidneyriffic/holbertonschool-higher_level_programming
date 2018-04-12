dd999206e92cd6bd6952bb425ab7324f4fc16b6c
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-04-12 14:17:53 -0700

    Add 1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+from calculator_1 import add
+from calculator_1 import sub
+from calculator_1 import div
+from calculator_1 import mul
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))

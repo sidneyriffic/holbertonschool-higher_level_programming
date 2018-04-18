1c8d60929a541b3f06cc566535a524a38916a5ee
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-04-17 21:19:25 -0700

    Add 0x03-13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+listint_t *is_pal_recurs(listint_t *left, listint_t *right)
+{
+	if (right->next == NULL)
+		return (left->next);
+	right = right->next;
+	if (right->next == NULL)
+	{
+		if (left->n == left->next->n)
+			return (left->next->next);
+		return (NULL);
+	}
+	right = is_pal_recurs(left->next, right->next);
+	if (right == NULL)
+		return (NULL);
+	if (right->n == left->n)
+	{
+		if (right->next == NULL)
+			return (left);
+		return (right->next);
+	}
+	return (NULL);
+}
+		
+
+int is_palindrome(listint_t **head)
+{
+	if (head == NULL || *head == NULL)
+		return (1);
+
+	if (is_pal_recurs(*head, *head) != NULL)
+		return (1);
+	return (0);
+}

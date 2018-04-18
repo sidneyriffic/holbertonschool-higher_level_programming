fc54979ba6e48af443dd55a5c01a81dc72833153
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-04-17 19:49:23 -0700

    Add idx < 0 case to 0x03-11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if my_list is None or idx is None or idx >= len(my_list):
+    if my_list is None or idx is None or idx < 0 or idx >= len(my_list):
         return None
     del my_list[idx]
     return my_list

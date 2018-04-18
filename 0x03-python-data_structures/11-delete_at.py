5acccb629245575df1870202814ddc396832833c
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-04-17 19:51:33 -0700

    Return original list instead of None in 0x03-11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     if my_list is None or idx is None or idx < 0 or idx >= len(my_list):
-        return None
+        return my_list
     del my_list[idx]
     return my_list

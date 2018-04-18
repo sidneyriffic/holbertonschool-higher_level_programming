0bbef28664f16f66072c145320be30e6148797b9
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-04-17 18:51:41 -0700

    Use or for short circuiting in 0x03-4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if (my_list is None) | (idx is None) | (idx < 0) | idx > len(my_list):
+    if (my_list is None) or (idx is None) or (idx < 0) or (idx > len(my_list)):
         return my_list
     listcpy = my_list.copy()
     listcpy[idx] = element
     return listcpy
+my_list = None
+idx = 3
+new_element = 9
+new_list = new_in_list(my_list, idx, new_element)
+
+print(new_list)
+print(my_list)

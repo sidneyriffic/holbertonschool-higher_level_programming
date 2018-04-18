a8e95829031d2e0b8b922357a106ec53f1a97527
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-04-17 19:09:47 -0700

    Fix style for 0x03-6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,8 +4,8 @@ def print_matrix_integer(matrix=[[]]):
         return None
     for row in matrix:
         if row is not None and len(row) > 0 and row[0] is not None:
-            print("{:d}".format(row[0]), end = '')
+            print("{:d}".format(row[0]), end='')
             if len(row) > 1:
                 for col in row[1:]:
-                    print(" {:d}".format(col), end ='')
+                    print(" {:d}".format(col), end='')
         print()

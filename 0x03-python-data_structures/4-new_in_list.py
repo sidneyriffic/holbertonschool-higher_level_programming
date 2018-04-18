97681006ba32215aa0928eb44c25c0386f8ceec7
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-04-17 18:48:57 -0700

    Fix error checks as with previous in 0x03-4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 | idx > my_list.len():
+    if (my_list is None) | (idx is None) | (idx < 0) | idx > len(my_list):
         return my_list
     listcpy = my_list.copy()
     listcpy[idx] = element

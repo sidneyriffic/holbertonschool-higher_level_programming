cfa7b9fc08e8588426c94779b50c8100fb1ac397
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-04-17 18:38:21 -0700

    Fix len function call in 0x03-1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if (my_list is None) or (idx is none) or (idx < 0) or (idx >= my_list.len()):
+    if (my_list is None) or (idx is None)\
+       or (idx < 0) or (idx >= len(my_list)):
         return
     return my_list[idx]
-

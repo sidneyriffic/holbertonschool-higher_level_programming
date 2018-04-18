4cc7c133d02cd13d2a381544dac9ecfe05e487a9
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-04-17 19:08:33 -0700

    Fix None cases in 0x03-6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix is None:
+        return None
     for row in matrix:
-        if (row):
+        if row is not None and len(row) > 0 and row[0] is not None:
             print("{:d}".format(row[0]), end = '')
-        for col in row[1:]:
-            print(" {:d}".format(col), end ='')
+            if len(row) > 1:
+                for col in row[1:]:
+                    print(" {:d}".format(col), end ='')
         print()
-matrix = [[1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()

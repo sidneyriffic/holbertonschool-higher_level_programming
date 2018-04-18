4e910bc4d789ebe141ba94133bda0ed344b1f1ce
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-04-17 18:52:04 -0700

    Remove debug from 0x03-4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,10 +5,3 @@ def new_in_list(my_list, idx, element):
     listcpy = my_list.copy()
     listcpy[idx] = element
     return listcpy
-my_list = None
-idx = 3
-new_element = 9
-new_list = new_in_list(my_list, idx, new_element)
-
-print(new_list)
-print(my_list)

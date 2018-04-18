7a6032b2ff5f7c8dde8c3c556674f33629b169c5
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-04-17 19:13:59 -0700

    Add None cases to 0x03-7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 1:
+    if tuple_a is None or tuple_b is None:
+        return None
+    if len(tuple_a) < 1 or tuple_a[0] is None:
         addone = 0
     else:
         addone = tuple_a[0]
-    if len(tuple_b) < 1:
+    if len(tuple_b) < 1 or tuple_b[0] is None:
         addtwo = 0
     else:
         addtwo = tuple_b[0]
     f = addone + addtwo
-    if len(tuple_a) < 2:
+    if len(tuple_a) < 2 or tuple_a[1] is None:
         addone = 0
     else:
         addone = tuple_a[1]
-    if len(tuple_b) < 2:
+    if len(tuple_b) < 2 or tuple_b[1] is None:
         addtwo = 0
     else:
         addtwo = tuple_b[1]
     s = addone + addtwo
     return (f, s)
 tuple_a = (1, 89)
-tuple_b = (88, 11)
+tuple_b = (88, None)
 new_tuple = add_tuple(tuple_a, tuple_b)
 print(new_tuple)
 
 print(add_tuple(tuple_a, (1, )))
-print(add_tuple(tuple_a, ()))
+print(add_tuple(tuple_a, None))

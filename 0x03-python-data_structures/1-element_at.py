0dc49e9e26226d682ed62dd0a760ef53400ad9b7
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-04-17 18:35:16 -0700

    Remove debug from 0x03-1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,4 +3,4 @@ def element_at(my_list, idx):
     if (my_list is None) or (idx is none) or (idx < 0) or (idx >= my_list.len()):
         return
     return my_list[idx]
-print(element_at(None, 1))
+

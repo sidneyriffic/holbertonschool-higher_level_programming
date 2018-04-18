34f5a98f2c6b4a5d1b6767d2fce87fd2a8dc3d97
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-04-17 18:59:05 -0700

    Fixx off-by-one in 0x03-4 from copying previous

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if (my_list is None) or (idx is None) or (element is None)\
-       or (idx < 0) or (idx > len(my_list)):
+       or (idx < 0) or (idx >= len(my_list)):
         return my_list
     listcpy = my_list.copy()
     listcpy[idx] = element

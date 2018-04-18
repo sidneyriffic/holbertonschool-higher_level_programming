d3b7d709b7bda3cc4b6dfc181beac9ae8f44ac76
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-04-17 19:15:03 -0700

    Remove debug from 0x03-7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -21,10 +21,3 @@ def add_tuple(tuple_a=(), tuple_b=()):
         addtwo = tuple_b[1]
     s = addone + addtwo
     return (f, s)
-tuple_a = (1, 89)
-tuple_b = (88, None)
-new_tuple = add_tuple(tuple_a, tuple_b)
-print(new_tuple)
-
-print(add_tuple(tuple_a, (1, )))
-print(add_tuple(tuple_a, None))

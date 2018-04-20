68b3ecab8d5bfa0c2be0ad23366ca6dfe5d2e4e5
Sidney Burkett <sidneyriffic@gmail.com>
2018-04-19 23:27:38 -0700

    Add 0x04-103

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,52 @@
+#include "Python.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_python_bytes - print information about a bytes object
+ *
+ * @p: bytes object
+ *
+ * Return: void
+ */
+void print_python_bytes(PyObject *p)
+{
+	PyBytesObject *bytes = (PyBytesObject *) p;
+	int len = PyBytes_Size(p);
+	char *ptr = bytes->ob_sval;
+
+	printf("size: %d\n trying string: %s\n", len, bytes->ob_sval);
+	len++;
+	if (len > 10)
+		len = 10;
+	printf("First %d bytes:", len);
+	while (len--)
+		printf(" %.2x", *ptr++);
+	printf("\n");
+}
+
+/**
+ * print_python_list - prints information about a python list
+ * and extra info on bytes objects
+ *
+ * @p: list to print information about
+ *
+ * Return: void
+ */
+void print_python_list(PyObject *p)
+{
+	PyListObject *lst = (PyListObject *) p;
+	long long ct = -1, size;
+	const char *type;
+
+	size = (long long) PyList_Size(p);
+	printf("[*] Size of the Python List = %lld\n", size);
+	printf("[*] Allocated = %lld\n", (long long) lst->allocated);
+	while (++ct < size)
+	{
+		type = lst->ob_item[ct]->ob_type->tp_name;
+		printf("Element %lld: %s\n", ct, type);
+		if (strcmp(type, "bytes") == 0)
+			print_python_bytes(lst->ob_item[ct]);
+	}
+}

b1f8045cfb2176d3bac8583f0edc5a3b25ae701c
Sidney Burkett <sidneyriffic@gmail.com>
2018-04-19 22:10:17 -0700

    Fix style in 0x04-12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,8 +4,8 @@ def roman_to_int(roman_string):
         return 0
     sumtot = 0
     prevval = 1000
-    romandict = {"I": 1, "V": 5, "X":10, "L": 50,\
-                  "C": 100, "D": 500, "M": 1000}
+    romandict = {"I": 1, "V": 5, "X": 10, "L": 50,
+                 "C": 100, "D": 500, "M": 1000}
     numbers = [romandict[x] for x in roman_string]
     for x in numbers:
         if x > prevval:

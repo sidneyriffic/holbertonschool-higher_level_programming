117a60b7e5c5fe1e575b50b64d001fa92515d5d1
Sidney Burkett <sidneyriffic@gmail.com>
2018-04-19 22:09:07 -0700

    Add 0x04-12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is None or type(roman_string) is not str:
+        return 0
+    sumtot = 0
+    prevval = 1000
+    romandict = {"I": 1, "V": 5, "X":10, "L": 50,\
+                  "C": 100, "D": 500, "M": 1000}
+    numbers = [romandict[x] for x in roman_string]
+    for x in numbers:
+        if x > prevval:
+            sumtot -= prevval * 2
+        prevval = x
+        sumtot += x
+    return sumtot

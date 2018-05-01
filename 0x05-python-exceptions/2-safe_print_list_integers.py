59fa17cab0657167a1e257db054d456d6a67cc44
Sidney Burkett <sidneyriffic@gmail.com>
2018-04-30 20:00:48 -0700

    Fix extra spaces around assignment of print parameter

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,7 +2,7 @@
 def safe_print_list_integers(my_list=[], x=0):
     try:
         for y in range(x):
-            print("{:d}".format(my_list[y]), end = "")
+            print("{:d}".format(my_list[y]), end="")
     except ValueError:
         return y
     except IndexError:

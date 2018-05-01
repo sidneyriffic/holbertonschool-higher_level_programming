c11c7ece051862f03083b8d870de0ecb2ec55c89
Sidney Burkett <sidneyriffic@gmail.com>
2018-04-30 20:57:30 -0700

    Remove debug code from 0x05-100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -8,19 +8,3 @@ def safe_print_integer_err(value):
         print("Exception: {}".format(x), file=sys.stderr)
         return False
     return True
-
-value = 89
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = -89
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = "Holberton"
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-    

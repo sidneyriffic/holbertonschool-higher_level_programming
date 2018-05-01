da29b5a33012aa4ac1fa405617cf9ae7c7cedfdb
Sidney Burkett <sidneyriffic@gmail.com>
2018-04-30 20:32:44 -0700

    Do not handle indexerror in 0x05-2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,10 +1,25 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        for y in range(x):
+    printed = 0
+    for y in range(x):
+        try:
             print("{:d}".format(my_list[y]), end="")
-    except:
-        return y
-    finally:
-        print()
-    return x
+            printed += 1
+        except ValueError:
+            continue
+        except TypeError:
+            continue
+    print()
+    return printed
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list_integers(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+
+my_list = [1, 2, 3, "Holberton", 4, 5, [1, 2, 3]]
+nb_print = safe_print_list_integers(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+
+nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))

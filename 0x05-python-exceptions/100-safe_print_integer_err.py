e34980a5a6d9b491654b2a8c3fd65b691795d7d5
Sidney Burkett <sidneyriffic@gmail.com>
2018-04-30 21:00:12 -0700

    Handle all exceptions

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,7 +4,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except ValueError as x:
+    except BaseException as x:
         print("Exception: {}".format(x), file=sys.stderr)
         return False
     return True

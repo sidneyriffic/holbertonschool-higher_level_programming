140f2c8efb69a3914d8cb3259a0c03e3c26b3e00
Sidney Burkett <sidneyriffic@gmail.com>
2018-05-01 21:16:55 -0700

    Pep8 0x06-6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -43,9 +43,11 @@ class Square:
     @position.setter
     def position(self, value):
         """Set position of square"""
-        if type(value) is not tuple or len(value) != 2 or type(value[0]) is not int\
-           or type(value[1]) is not int or value[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of two positive integers")
+        if type(value) is not tuple or len(value) != 2\
+           or type(value[0]) is not int or type(value[1]) is not int\
+           or value[0] < 0 or value[1] < 0:
+            raise TypeError(
+                "position must be a tuple of two positive integers")
         self.__position = value
 
     def my_print(self):

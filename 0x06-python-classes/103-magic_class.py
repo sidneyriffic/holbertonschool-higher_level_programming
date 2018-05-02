5a8e51fd196156e0bbbc280b8c29e85fea9b6099
Sidney Burkett <sidneyriffic@gmail.com>
2018-05-01 22:46:17 -0700

    Set radius to 0 even if typeerror in 0x06-103

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -3,6 +3,7 @@
 
 class MagicClass:
     def __init(self, radius=0):
+        self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
         self.__radius = radius

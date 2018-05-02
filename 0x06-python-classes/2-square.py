0c97635825701324b7f2d5e0bcf2c1ac41403d59
Sidney Burkett <sidneyriffic@gmail.com>
2018-05-01 19:56:23 -0700

    Add 0x06-2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines the square class"""
+
+
+class Square:
+    """Square class. Has a size"""
+    def __init__(self, size=0):
+        try:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+        except TypeError:
+            raise TypeError("size must be an integer")
+        self.__size = size

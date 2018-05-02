4abef09fa62d440bb4aa48745aa148f7e31b9f22
Sidney Burkett <sidneyriffic@gmail.com>
2018-05-01 20:04:30 -0700

    Try isinstance instead of type is not

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Square class. Has a size"""
     def __init__(self, size=0):
-        if type(size) is not int:
+        if isinstance(size, int):
             raise TypeError("Size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")

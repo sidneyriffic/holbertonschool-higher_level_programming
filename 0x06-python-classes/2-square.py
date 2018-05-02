f30982a054324053f7ca7c223a19c1944b9e0d70
Sidney Burkett <sidneyriffic@gmail.com>
2018-05-01 20:12:55 -0700

    Don't allow float sizes for 0x06-2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,9 +5,8 @@
 class Square:
     """Square class. Has a size"""
     def __init__(self, size=0):
-        try:
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = size
-        except TypeError:
-                raise TypeError("size must be an integer")
+        if type(size) is not int:
+            raise TypeError("size must be an integer")            
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size

987be0d258e9726bb56034a625018a8df9a24e2c
Sidney Burkett <sidneyriffic@gmail.com>
2018-05-02 05:55:04 -0700

    Add trailing __ to init in 0x06-103

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -5,7 +5,7 @@
 class MagicClass:
     """A circle class"""
 
-    def __init(self, radius=0):
+    def __init__(self, radius=0):
         """Initialized circle"""
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:

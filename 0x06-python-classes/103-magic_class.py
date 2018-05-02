ebba91ef09c766c0f5797dcbe30e72dc7fdbfc55
Sidney Burkett <sidneyriffic@gmail.com>
2018-05-01 22:55:21 -0700

    Try to match exact code instead of function for 0x06-103

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
+"""a circle class"""
 
 
 class MagicClass:
+    """A circle class"""
+
     def __init(self, radius=0):
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
-        self.__radius = radius
+        else:
+            self.__radius = radius
 
     def area(self):
+        """Return area of circle"""
         return self.__radius ** 2 * math.pi
 
     def circumference(self):
+        """Return circumference of circle"""
         return 2 * math.pi * self.__radius

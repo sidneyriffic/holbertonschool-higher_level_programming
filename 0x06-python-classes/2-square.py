c314d653a251a35b2229d7be69f5fa593ba327c5
Sidney Burkett <sidneyriffic@gmail.com>
2018-05-01 20:07:03 -0700

    Don't use explicit type check

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,8 +5,9 @@
 class Square:
     """Square class. Has a size"""
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("Size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        try:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+        except TypeError:
+                raise TypeError("Size must be an integer")

53d207a300797f1897b351ddb455abc9ffff057a
Sidney Burkett <sidneyriffic@gmail.com>
2018-05-01 20:02:48 -0700

    Use a pythonicier way of checking type

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Square class. Has a size"""
     def __init__(self, size=0):
-        if not isinstance(size, int):
+        if type(size) is not int:
             raise TypeError("Size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")

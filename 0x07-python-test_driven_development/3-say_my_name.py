672c8f7ee70d0b4b431e2fa62d930f80cd00ec1e
Sidney Burkett <sidneyriffic@gmail.com>
2018-05-06 01:57:54 -0700

    Fix exceptions in 0x07-3 on second reading

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -6,11 +6,6 @@ def say_my_name(first_name, last_name=""):
     """Prints two strings as a name"""
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    print("My name is", first_name, end=" ")
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-    print(last_name)
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/3-say_my_name.txt")
+    print("My name is", first_name, last_name)

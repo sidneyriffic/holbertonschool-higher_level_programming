a015aabc5e564c94e437a874a6296675923157c0
Sidney Burkett <sidneyriffic@gmail.com>
2018-05-05 21:20:36 -0700

    Update 0x07-3 for new requirements

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -4,10 +4,13 @@
 
 def say_my_name(first_name, last_name=""):
     """Prints two strings as a name"""
-    print("My name is", end=" ")
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    print(first_name, end=" ")
+    print("My name is", first_name, end=" ")
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
     print(last_name)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/3-say_my_name.txt")

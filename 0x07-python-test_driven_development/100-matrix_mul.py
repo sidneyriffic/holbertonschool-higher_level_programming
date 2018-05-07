001e6acf18c8db50ebecb5bb82c8c5d04411c0f8
Sidney Burkett <sidneyriffic@gmail.com>
2018-05-06 17:59:41 -0700

    Update 0x07-100 in accordance with task changes

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -3,10 +3,17 @@
 
 
 def matrix_mul(m_a, m_b):
+    """Multiplies two matrices"""
     if type(m_a) is not list:
         raise TypeError("m_a must be a list")
     if type(m_b) is not list:
         raise TypeError("m_b must be a list")
+    for x in m_a:
+        if type(x) is not list:
+            raise TypeError("m_a must be a list of lists")
+    for x in m_b:
+        if type(x) is not list:
+            raise TypeError("m_b must be a list of lists")
     if len(m_a) < 1:
         raise ValueError("m_a can't be empty")
     if len(m_b) < 1:
@@ -17,18 +24,18 @@ def matrix_mul(m_a, m_b):
     browlen = len(m_b[0])
     if browlen < 1:
         raise ValueError("m_b can't be empty")
-    for row in m_b:
-        if len(row) != browlen:
-            raise TypeError("each row of m_b must should be of the same size")
-        for col in row:
-            if type(col) is not float and type(col) is not int:
-                raise TypeError("m_b should contain only integers or floats")
     for row in m_a:
         if len(row) != arowlen:
             raise TypeError("each row of m_a must should be of the same size")
         for col in row:
             if type(col) is not float and type(col) is not int:
                 raise TypeError("m_a should contain only integers or floats")
+    for row in m_b:
+        if len(row) != browlen:
+            raise TypeError("each row of m_b must should be of the same size")
+        for col in row:
+            if type(col) is not float and type(col) is not int:
+                raise TypeError("m_b should contain only integers or floats")
     if arowlen != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
     newmatrix = []

f7e29d7d9c214fe7ff9bf345320de01251d6bbea
Sidney Burkett <sidneyriffic@gmail.com>
2018-05-16 22:14:38 -0700

    Add 0x0B-11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Student class"""
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first, last, age):
+        self.first = first
+        self.last = last
+        self.age = age
+
+    def to_json(self):
+        """Returns student's serializable dict elements as a dict"""
+        retdict = {}
+        objdict = self.__dict__
+        for ele in objdict:
+            if type(objdict[ele]) in [list, dict, str, int, bool]:
+                retdict[ele] = objdict[ele]
+        return retdict

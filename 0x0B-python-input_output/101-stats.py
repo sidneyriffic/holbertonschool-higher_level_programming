37c24b2e167ed7b124ed59764356e3412d93ecee
Sidney Burkett <sidneyriffic@gmail.com>
2018-05-16 23:51:34 -0700

    Try exception instead of signal trap for 0x0B-101 checker

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,32 +1,26 @@
 #!/usr/bin/python3
 import sys
-import signal
 
 
 stdin = sys.stdin
 codes = {}
 totalsize = 0
 linesread = 0
-
-
-def sigint(signal, frame):
+try:
+    while True:
+        line = stdin.readline()
+        line = line.split(" ")
+        if line[-2] in codes:
+            codes[line[-2]] += 1
+        else:
+            codes[line[-2]] = 1
+        totalsize += int(line[-1])
+        linesread += 1
+        if linesread % 10 == 0:
+            print("File size:", totalsize)
+            for code in codes:
+                print("{}: {}\n".format(code, codes[code]))
+except KeyboardInterrupt:
     print("File size:", totalsize)
     for code in codes:
         print("{}: {}\n".format(code, codes[code]))
-    exit(0)
-
-signal.signal(signal.SIGINT, sigint)
-
-while True:
-    line = stdin.readline()
-    line = line.split(" ")
-    if line[-2] in codes:
-        codes[line[-2]] += 1
-    else:
-        codes[line[-2]] = 1
-    totalsize += int(line[-1])
-    linesread += 1
-    if linesread % 10 == 0:
-        print("File size:", totalsize)
-        for code in codes:
-            print("{}: {}\n".format(code, codes[code]))

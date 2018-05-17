85b10c35c4f7c47761b0cc8055a89c717167cbdd
Sidney Burkett <sidneyriffic@gmail.com>
2018-05-16 21:43:19 -0700

    Add extra line after import for PEP8 in 0x0B 3 and 4

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a function that writes a string to a file"""
 
+
 def write_file(filename="", text=""):
     """Writes a string to a given file"""
     with open(filename, "w") as f:
--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a function that appends a string to a file"""
 
+
 def append_write(filename="", text=""):
     """Writes a string to a given file"""
     with open(filename, "a") as f:

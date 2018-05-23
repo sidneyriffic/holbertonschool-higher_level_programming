52ec45207bfa131658200528be79578909722082
Sidney Burkett <sidneyriffic@gmail.com>
2018-05-22 21:55:13 -0700

    Add first test cases

--- 0x0C-python-almost_a_circle/tests/tests.py
+++ 0x0C-python-almost_a_circle/tests/tests.py
@@ -183,10 +183,115 @@ class TestRectangle(unittest.TestCase):
         global idct
         a = Rectangle(3, 4)
         idct += 1
-        assertEqual(a.area(), 12)
+        self.assertEqual(a.area(), 12)
 
     def testarea2(self):
         """tests rectangle area function"""
         a = Rectangle(4, 5, 100, 20, 10)
+        self.assertEqual(a.area(), 20)
+
+#test fewer args to inits
+class TestSquare(unittest.TestCase):
+    """Tests the square class"""
+
+    def testbasicrect(self):
+        """Makes a basic square with only valid width/height"""
+        global idct
+        a = Square(10)
         idct += 1
-        assertEqual(a.area(), 20)
+        self.assertEqual(a.id, idct)
+        self.assertEqual(a.width, 10)
+        self.assertEqual(a.height, 10)
+
+    def testnegsize(self):
+        """Tries a negative size"""
+        with self.assertRaises(ValueError) as e:
+            a = Square(-10)
+        self.assertEqual(e.exception.args[0], "width must be > 0")
+
+    def testzerosize(self):
+        """Tries a zero width"""
+        with self.assertRaises(ValueError) as e:
+            a = Square(0)
+        self.assertEqual(e.exception.args[0], "width must be > 0")
+
+    def testsqwithx(self):
+        """Make a square with only an x position"""
+        global idct
+        a = Square(10, 5)
+        idct += 1
+        self.assertEqual(a.id, idct)
+        self.assertEqual(a.width, 10)
+        self.assertEqual(a.height, 10)
+        self.assertEqual(a.x, 5)
+
+    def testsqwithy(self):
+        """Make a square with x and y position"""
+        global idct
+        a = Square(10, 5, 3)
+        idct += 1
+        self.assertEqual(a.id, idct)
+        self.assertEqual(a.width, 10)
+        self.assertEqual(a.height, 10)
+        self.assertEqual(a.x, 5)
+        self.assertEqual(a.y, 3)
+
+    def testnegx(self):
+        """Tries a negative x"""
+        with self.assertRaises(ValueError) as e:
+            a = Square(10, -1)
+        self.assertEqual(e.exception.args[0], "x must be >= 0")
+
+    def testnegy(self):
+        """Tries a negative y"""
+        with self.assertRaises(ValueError) as e:
+            a = Square(10, 1, -1)
+        self.assertEqual(e.exception.args[0], "y must be >= 0")
+
+    def testid(self):
+        """Test square construction given an id"""
+        a = Square(1, 2, 3, 4)
+        self.assertEqual(a.id, 4)
+        self.assertEqual(a.width, 1)
+        self.assertEqual(a.height, 1)
+        self.assertEqual(a.x, 2)
+        self.assertEqual(a.y, 3)
+
+    def testwidthtype(self):
+        """Test invalid types for size"""
+        for ele in notintegers:
+            with self.subTest(type=type(ele)):
+                with self.assertRaises(TypeError) as e:
+                    Square(ele)
+                self.assertEqual(e.exception.args[0],
+                                  "width must be an integer")
+
+    def testxtype(self):
+        """Test invalid types for x"""
+        for ele in notintegers:
+            with self.subTest(type=type(ele)):
+                with self.assertRaises(TypeError) as e:
+                    Square(4, ele)
+                self.assertEqual(e.exception.args[0],
+                                  "x must be an integer")
+
+    def testytype(self):
+        """Test invalid types for y"""
+        for ele in notintegers:
+            with self.subTest(type=type(ele)):
+                with self.assertRaises(TypeError) as e:
+                    Square(4, 5, ele)
+                self.assertEqual(e.exception.args[0],
+                                  "y must be an integer")
+
+    def testarea(self):
+        """Tests rectangle area function"""
+        global idct
+        a = Square(4)
+        idct += 1
+        self.assertEqual(a.area(), 16)
+
+    def testarea2(self):
+        """tests rectangle area function"""
+        a = Square(4, 100, 20, 10)
+        self.assertEqual(a.area(), 16)

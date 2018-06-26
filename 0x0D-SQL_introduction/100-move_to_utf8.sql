d069b8c685b474a747ef7e9b6b7316c8ad8ffb13
Sidney Burkett <sidneyriffic@gmail.com>
2018-06-26 14:00:07 -0700

    Add 0x0D-100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,8 @@
 -- convert database to UTF8
 -- convert database to UTF8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- select hbtn_0c_0 database
+USE hbtn_0c_0
 -- convert first_table to UTF8
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;

5a747a2be33122733299d72f73b0be42b9f182eb
Sidney Burkett <sidneyriffic@gmail.com>
2018-06-25 21:00:43 -0700

    Add 0x0D 7-8

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- insert an entry into first_table
+-- insert an entry into first_table
+INSERT INTO first_table VALUES (89, "Holberton School")
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,5 @@
+-- count entries with id=89
+-- count entries with id=89
+SELECT COUNT(id)
+FROM first_table
+WHERE id=89

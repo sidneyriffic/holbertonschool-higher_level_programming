4a4179b7d9ef0a1118123dc2fc4d2027c6100cb0
Sidney Burkett <sidneyriffic@gmail.com>
2018-06-25 22:03:40 -0700

    Add changes with real values

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,6 @@
 -- convert database to UTF8
 -- convert database to UTF8
-ALTER DATABASE databasename CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- convert first_table to UTF8
-ALTER TABLE tablename CONVERT TO CHARACTER SET utf8mb4
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;

2a5853e68084601c34c2327e451fe8a035bffdec
Sidney Burkett <sidneyriffic@gmail.com>
2018-06-26 13:56:55 -0700

    Fix error if table already exists for 0x0D-9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- create second_table and add some records
 -- create second_table
-CREATE TABLE second_table(id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
 -- add some records
 INSERT INTO second_table
 VALUES

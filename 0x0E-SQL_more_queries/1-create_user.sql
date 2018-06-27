c8b8d2bbec292f9278040eace0b1e027be5b828f
Sidney Burkett <sidneyriffic@gmail.com>
2018-06-26 23:21:53 -0700

    Add 0x0E-1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- create a user on server and assign all privileges
+-- create user
+CREATE USER IF NOT EXISTS user_0d_1;
+-- assign all privileges to everything
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1;

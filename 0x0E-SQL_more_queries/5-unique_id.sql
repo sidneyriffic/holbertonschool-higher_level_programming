cb26af90a17b968a5c871afabde04834c1d29fed
Sidney Burkett <sidneyriffic@gmail.com>
2018-06-26 23:49:14 -0700

    Fix table name in 0x0E 5-6

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- create a table called force_name
-CREATE TABLE IF NOT EXISTS id_not_null(id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id(id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
 -- create a table called force_name
 CREATE TABLE IF NOT EXISTS
-id_not_null(id INT AUTO_INCREMENT UNIQUE NOT NULL,
+states(id INT AUTO_INCREMENT UNIQUE NOT NULL,
 name VARCHAR(256), PRIMARY KEY (id));

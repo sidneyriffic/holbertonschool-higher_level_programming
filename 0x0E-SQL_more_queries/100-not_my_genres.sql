be78eb6d65f7992ec8cb74a973c1b4692b074f89
Sidney Burkett <sidneyriffic@gmail.com>
2018-06-27 01:29:24 -0700

    Add 0x0E-100

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,6 @@
+-- show all genres for the show "Dexter"
+SELECT name FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
+AND tv_shows.title="Dexter"
+RIGHT JOIN tv_genres ON tv_show_genres.genre_id=tv_genres.id
+WHERE genre_id IS NULL

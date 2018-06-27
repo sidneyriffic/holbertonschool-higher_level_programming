15a31812ef6038d4b89beec2637f5f116d829e4b
Sidney Burkett <sidneyriffic@gmail.com>
2018-06-26 23:51:53 -0700

    Create database in 0x0E-6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,9 @@
--- create a table called force_name
+-- create a database called hbtn_0d_usa and table called states
+-- create hbtn_0d_usa database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- use the new table
+USE hbtn_0d_usa;
+-- create states table
 CREATE TABLE IF NOT EXISTS
 states(id INT AUTO_INCREMENT UNIQUE NOT NULL,
 name VARCHAR(256), PRIMARY KEY (id));

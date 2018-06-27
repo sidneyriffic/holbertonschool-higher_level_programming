085ae00d9b7a0323572c6ddcc765367756c9b44c
Sidney Burkett <sidneyriffic@gmail.com>
2018-06-26 23:16:11 -0700

    Add FOR keyword to lines in 0x0E-0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- show grants for two users
 -- show grants for user_0d_1
-SHOW GRANTS user_0d_1;
+SHOW GRANTS FOR user_0d_1@localhost;
 -- show grants for user_0d_2
-SHOW GRANTS user_0d_2;
+SHOW GRANTS FOR user_0d_2@localhost;

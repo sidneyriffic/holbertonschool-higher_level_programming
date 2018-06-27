793ebeab585fb6b32bd8db91a7c20916aeabc453
Sidney Burkett <sidneyriffic@gmail.com>
2018-06-27 01:32:46 -0700

    Fix keyword caps in 0x0E-100

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,5 +1,5 @@
 -- show all genres for the show "Dexter"
-SELECT tv_genres.name as name FROM tv_shows
+SELECT tv_genres.name AS name FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
 AND tv_shows.title="Dexter"
 RIGHT JOIN tv_genres ON tv_show_genres.genre_id=tv_genres.id

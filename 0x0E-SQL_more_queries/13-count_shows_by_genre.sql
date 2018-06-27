c62252f4ba91606b545544e609bfcc8122bd08fa
Sidney Burkett <sidneyriffic@gmail.com>
2018-06-27 00:44:15 -0700

    Add HAVING clause to 0x0E-13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,4 @@
 -- get a count of shows by genre
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_shows
 FROM tv_show_genres LEFT JOIN tv_genres ON tv_show_genres.genre_id=tv_genres.id
-GROUP BY genre ORDER BY number_shows DESC
+GROUP BY genre HAVING number_shows > 0 ORDER BY number_shows DESC

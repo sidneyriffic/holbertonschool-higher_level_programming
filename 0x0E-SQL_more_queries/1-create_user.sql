a63a00554be450745ec4896391295e7e5ec01625
Sidney Burkett <sidneyriffic@gmail.com>
2018-06-26 23:27:07 -0700

    Revise 0x0E-1 to specify localhost

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- create a user on server and assign all privileges
 -- create user
-CREATE USER IF NOT EXISTS user_0d_1;
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
 -- assign all privileges to everything
-GRANT ALL PRIVILEGES ON * . * TO user_0d_1;
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;

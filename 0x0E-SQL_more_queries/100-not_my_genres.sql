727022f81d9c926400d630d7bb2f72b5c5d32092
Sidney Burkett <sidneyriffic@gmail.com>
2018-06-27 01:31:51 -0700

    Sort list for 0x0E-100

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,6 +1,6 @@
 -- show all genres for the show "Dexter"
-SELECT name FROM tv_shows
+SELECT tv_genres.name as name FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
 AND tv_shows.title="Dexter"
 RIGHT JOIN tv_genres ON tv_show_genres.genre_id=tv_genres.id
-WHERE genre_id IS NULL
+WHERE genre_id IS NULL ORDER BY name

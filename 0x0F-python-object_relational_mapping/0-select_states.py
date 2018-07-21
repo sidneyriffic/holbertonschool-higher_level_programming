a2b693a399f14aca3c01743a1557fa8b32b84967
Sidney Burkett <sidneyriffic@gmail.com>
2018-07-20 22:01:08 -0700

    Hardcode port 3306 for SQL connection in 0x0F-0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     from sys import argv
 
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
     Base = declarative_base()

4db4b2f8731f760ee127ca49cc5aa72a350dc27d
Sidney Burkett <sidneyriffic@gmail.com>
2018-07-20 22:48:01 -0700

    Pep8 0x0F-0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-#List all states using mysqldb
+"""List all states using mysqldb"""
 
 
 if __name__ == "__main__":
     import MySQLdb
     from sys import argv
-
 
     db = MySQLdb.connect(host="localhost", user=argv[1],
                          passwd=argv[2], db=argv[3])

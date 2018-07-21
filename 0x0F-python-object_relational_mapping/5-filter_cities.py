12cebc5cd4f0c527185ec719ffd44abfa8910572
Sidney Burkett <sidneyriffic@gmail.com>
2018-07-20 23:42:37 -0700

    Add 0x0F-5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""List all states using mysqldb"""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                         passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT cities.name FROM cities"
+                " JOIN states ON states.id=cities.state_id "
+                "WHERE states.name=%s ORDER BY cities.id", (argv[4],))
+    print(", ".join([x[0] for x in cur.fetchall()]))

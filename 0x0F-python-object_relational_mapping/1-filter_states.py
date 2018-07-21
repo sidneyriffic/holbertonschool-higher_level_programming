9b1e84f519937a859df94672d5b22ef1af85d494
Sidney Burkett <sidneyriffic@gmail.com>
2018-07-20 22:53:46 -0700

    Add 0x0F-1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""List all states using mysqldb"""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                         passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states WHERE SUBSTR(name, 1, 1)='N'")
+    for row in cur.fetchall():
+        print("({}, '{}')".format(row[0], row[1]))

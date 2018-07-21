1dd110e7f8a0ec74089ae603eb867c2917784008
Sidney Burkett <sidneyriffic@gmail.com>
2018-07-20 21:39:24 -0700

    Pep8 and import MySQLdb even though it's not used in 0x0F-0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
 """Select all states in hbtn_0e_0_usa"""
 
-#import sqlalchemy as sqla
 from sqlalchemy import Integer, String, create_engine, Column
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-#import MySQLdb
+import MySQLdb
 from sys import argv
 
 
 engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                            format(argv[1], argv[2], argv[3]),
-                            pool_pre_ping=True)
+                       format(argv[1], argv[2], argv[3]),
+                       pool_pre_ping=True)
 Base = declarative_base()
 Base.metadata.create_all(engine)
 
+
 class State(Base):
+    """Class to states table"""
     __tablename__ = "states"
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
+
 
 Session = sessionmaker(bind=engine)
 

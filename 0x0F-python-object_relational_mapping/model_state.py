2262fcc76704fee339798eba8c0f423584ce4b3d
Sidney Burkett <sidneyriffic@gmail.com>
2018-07-21 20:01:20 -0700

    Add 0x0F-6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Select all states in hbtn_0e_0_usa"""
+
+
+from sqlalchemy import Integer, String, Column
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+class State(Base):
+    """Class to states table"""
+    __tablename__ = "states"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)

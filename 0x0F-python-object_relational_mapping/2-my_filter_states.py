824378e45210fa1fc69d669902c0e925ea105a0c
Sidney Burkett <sidneyriffic@gmail.com>
2018-07-21 19:39:08 -0700

    Fix case insensitivity in queries for 0x0F 2-3

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT id, name FROM states "
-                "WHERE name='" + argv[4] + "' ORDER BY id")
+    cur.execute("SELECT id, name FROM states WHERE name='" +
+                argv[4] + "' COLLATE latin1_general_cs ORDER BY id")
     for row in cur.fetchall():
         print("({}, '{}')".format(row[0], row[1]))
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT id, name FROM states "
-                "WHERE name=%s ORDER BY id", (argv[4],))
+    cur.execute("SELECT id, name FROM states WHERE name=%s "
+                "COLLATE latin1_general_cs ORDER BY id", (argv[4],))
     for row in cur.fetchall():
         print("({}, '{}')".format(row[0], row[1]))

0d4e15288b727f03dac8c2fd67a6c740061ff3e3
Sidney Burkett <sidneyriffic@gmail.com>
2018-07-21 21:38:57 -0700

    Add Louisiana specifically for 0x0F-11

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -16,8 +16,8 @@ if __name__ == "__main__":
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    session.add(State(name=argv[4]))
+    session.add(State(name="Louisiana"))
     session.commit()
     qry = session.query(State).filter(State.name ==
-                                      func.binary(argv[4])).first()
+                                      func.binary("Louisiana")).first()
     print("{}".format(qry.id))

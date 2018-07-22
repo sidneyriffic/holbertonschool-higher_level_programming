ce1a88132012433e842da875a92712a583d8cdb0
Sidney Burkett <sidneyriffic@gmail.com>
2018-07-21 22:46:03 -0700

    Add model_city.py for 0x0F-14

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Model file for cities"""
+
+
+from model_state import Base, State
+from sqlalchemy import Integer, String, Column, ForeignKey
+
+class City(Base):
+    """City model class"""
+
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"))

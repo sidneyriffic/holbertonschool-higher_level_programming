8cc2600c51c44c205311ea73ccefe0c9cb10f38c
Sidney Burkett <sidneyriffic@gmail.com>
2018-08-03 15:37:24 -0700

    Cut 2- instead of 2 in 0x10-3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # take URL as arg then curl get and print size of body
-curl -sI -X OPTIONS "$1" | grep "Allow: " | cut -f 2 -d " "
+curl -X "DELETE" "$1"curl -sI -X OPTIONS "$1" | grep "Allow:" | cut -f 2- -d " "

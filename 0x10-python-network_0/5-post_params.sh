634ba18c71cccbf7061468dcf6161b2f03b4b2c0
Sidney Burkett <sidneyriffic@gmail.com>
2018-08-03 23:54:28 -0700

    Make 0x10-5 silent

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # take URL as arg then curl post with custom fields
-curl -d"email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
+curl -sd"email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"

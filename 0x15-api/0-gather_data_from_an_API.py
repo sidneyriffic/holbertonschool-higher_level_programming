ff98c6cc6a6d9d2f59ce36850ce3d04a404b295f
Sidney Burkett <sidneyriffic@gmail.com>
2018-08-27 21:50:55 -0700

    Add 0x15-0 and readme

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Get information from the JSONplaceholder API"""
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    user = requests.get("http://jsonplaceholder.typicode.com/users/"
+                        + argv[1]).json()
+    todos = requests.get("http://jsonplaceholder.typicode.com/todos?userId="
+                         + argv[1]).json()
+    done = requests.get("http://jsonplaceholder.typicode.com/todos?userId="
+                         + argv[1] + "&completed=true").json()
+    
+    print("Employee {} is done with tasks({}/{})"\
+          .format(user["name"], len(done), len(todos)))
+    for task in done:
+        print("\t" + task["title"])
---
+++ 0x15-api/README.md
@@ -0,0 +1,5 @@
+### Get information from the JSONplaceholder api
+0-gather_data_from_an_API.py: get a user's TODO  
+1-export_to_CSV.py: export the TODO information to CSV  
+2-export_to_JSON.py: export the TODO information to JSON  
+3-dictionary_of_lists_of_dictionaries.py: as 2, but for all employees  

f36c9716aabfd56a03f1070e58bfa39c66c83e4a
Sidney Burkett <sidneyriffic@gmail.com>
2018-09-05 20:21:36 -0700

    Take a full url not star wars film id for 0x14-2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 
-request.get('https://swapi.co/api/films/' + process.argv[2], function (err, res, body) {
+request.get(process.argv[2], function (err, res, body) {
   if (err) console.log(err);
   else console.log('code: ' + res.statusCode);
 });

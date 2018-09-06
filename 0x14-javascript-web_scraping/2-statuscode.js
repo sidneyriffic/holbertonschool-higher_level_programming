414033168bec9aba48acba4e251b324f3e80f24c
Sidney Burkett <sidneyriffic@gmail.com>
2018-09-05 20:20:04 -0700

    Add 'code :' to the front of output for 0x14-2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,5 @@ const request = require('request');
 
 request.get('https://swapi.co/api/films/' + process.argv[2], function (err, res, body) {
   if (err) console.log(err);
-  else console.log(res.statusCode);
+  else console.log('code: ' + res.statusCode);
 });

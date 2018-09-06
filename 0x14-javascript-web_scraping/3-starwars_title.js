1e61215bbb2894d58321eabe91cff1bf3f6e6553
Sidney Burkett <sidneyriffic@gmail.com>
2018-09-05 20:11:29 -0700

    Move previous 0x14-2 to 3 and add correct 2

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,5 +4,5 @@ const request = require('request');
 
 request('https://swapi.co/api/films/' + process.argv[2], function (err, res, body) {
   if (err) console.log(err);
-  else console.log(res.statusCode);
+  else console.log(JSON.parse(body).title);
 });

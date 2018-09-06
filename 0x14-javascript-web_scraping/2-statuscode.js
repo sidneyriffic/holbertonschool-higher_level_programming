6e82aa314b4b72ec3bcac52cefa0530aa5462553
Sidney Burkett <sidneyriffic@gmail.com>
2018-09-05 20:18:27 -0700

    Try explicit get http method for 0x14-2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 
-request('https://swapi.co/api/films/' + process.argv[2], function (err, res, body) {
+request.get('https://swapi.co/api/films/' + process.argv[2], function (err, res, body) {
   if (err) console.log(err);
   else console.log(res.statusCode);
 });

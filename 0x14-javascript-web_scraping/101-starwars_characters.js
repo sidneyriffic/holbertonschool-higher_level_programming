d14241d65e5a1295b1bc6f232c54596f83a89e04
Sidney Burkett <sidneyriffic@gmail.com>
2018-09-06 01:24:11 -0700

    Chance 0x14-101 to faster Promise.all() methodology

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,27 +2,26 @@
 
 const request = require('request');
 
-request('http://swapi.co/api/films/' + process.argv[2], async function (err, res, body) {
+request('http://swapi.co/api/films/' + process.argv[2], function (err, res, body) {
   if (err) console.log(err);
   else {
     let characters = JSON.parse(body).characters;
-    const listpromise = new Promise(async resolve => {
-      let charnames = [];
-      for (let character in characters) {
-        let charname = '';
-        const namepromise = new Promise(resolve => {
-          request(characters[character], function (err, res, body) {
-            if (err) console.log(err);
-            else {
-              console.log(JSON.parse(body).name);
-            }
-            resolve(charname);
-          });
+    let promiselist = [];
+    for (let character in characters) {
+      const namepromise = new Promise(resolve => {
+        request(characters[character], function (err, res, body) {
+          if (err) console.log(err);
+          else {
+            resolve(JSON.parse(body).name);
+          }
         });
-        await namepromise;
+      });
+      promiselist.push(namepromise);
+    }
+    Promise.all(promiselist).then(function (namelist) {
+      for (let name in namelist) {
+        console.log(namelist[name]);
       }
-      resolve(charnames);
     });
-    await listpromise;
   }
 });

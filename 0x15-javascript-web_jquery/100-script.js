ba273762f42b97d309535d88a648d34ccbf770b2
Sidney Burkett <sidneyriffic@gmail.com>
2018-09-08 02:53:02 -0700

    Semistandard 0x15-100

--- 0x15-javascript-web_jquery/100-script.js
+++ 0x15-javascript-web_jquery/100-script.js
@@ -1,3 +1,3 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
+document.addEventListener('DOMContentLoaded', function (event) {
   document.querySelector('header').style.color = 'red';
 });

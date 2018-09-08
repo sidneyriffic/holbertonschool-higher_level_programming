d0c46cc5ed2bfd1c8d75ce4a2a7604fc4f3f1c5c
Sidney Burkett <sidneyriffic@gmail.com>
2018-09-08 03:10:53 -0700

    Add 0x15-103

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,17 @@
+function getspeed () {
+  $.ajax({
+    url: 'https://query.yahooapis.com/v1/public/yql?q=select%20wind%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22' + $('input#city_search').val() + '%22)&format=json',
+    type: 'GET',
+    success: result => {
+      $('div#wind_speed').text(result.query.results.channel.wind.speed);
+    }
+  });
+}
+
+$(document).ready(function () {
+  $('input#btn_search').click(getspeed());
+  $('input#city_search').keypress(function (event) {
+    console.log(event);
+    if (event.originalEvent.key === 'Enter') getspeed();
+  });
+});
